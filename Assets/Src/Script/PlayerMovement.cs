5ed41d1a899cd9f600840d62fdbd2e9b94122ee0
Mao JunJie <jasonmao0802@gmail.com>
2025-03-05 13:46:29 -0500

    update

--- Assets/Src/Script/PlayerMovement.cs
+++ Assets/Src/Script/PlayerMovement.cs
@@ -12,19 +12,18 @@ public class PlayerMovement : MonoBehaviour
     float sprintFactor = 1.5f;
     
     [SerializeField]
-    float normalSpeed, acceleration, jump, frictionAmount;
+    float normalSpeed, maxSpeed, acceleration, jump, groundDecceleration, airDecceleration;
     [SerializeField]
     Transform Cam;
 
     [SerializeField, Range(0f, .1f)]
     float turnSmoothTime;
 
-
-    [SerializeField]
-    float decceleration;
-
     [SerializeField, Range(0f, 1f)] float jumpBufferTime = 0.2f;
     [SerializeField, Range(0f, 1f)] float releaseBufferTime = 0.2f;
+
+    [SerializeField, Range(0f, 90f)]
+    float maxGroundAngle = 25f;
 
 
     [SerializeField]
@@ -43,18 +42,27 @@ public class PlayerMovement : MonoBehaviour
     float jumpBufferCounter;
     float extraHeightTest = .2f;
     float offset = 0.1f;
+    float minGroundDotProduct;
 
+    bool _Grounded = false;
     bool _jump = false;
     bool _jumpReleased = false;
     Vector3 movement;
+    Vector3 contactNormal;
 
     Vector3 originalposition = new Vector3(0f, 1.55999994f, 0f);
     Quaternion originalrotation = Quaternion.identity;
     Vector3 originalscale = Vector3.one;
-   
+
+    private void OnValidate()
+    {
+        minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
+        OnValidate();
         speed = normalSpeed;
         rb = gameObject.GetComponent<Rigidbody>();
         velPower = 0.7f;
@@ -80,6 +88,8 @@ public class PlayerMovement : MonoBehaviour
         bool JumpButtonUp = Input.GetButtonUp("Jump");
         Vector3 direction = new Vector3(MoveValue.x, 0.0f, MoveValue.y).normalized;
 
+        _Grounded = IsGrounded();
+        
         SimulateMovement(direction.x, direction.z, SprintAction.IsPressed());
         SimulateJump(JumpButtonDown, JumpButtonUp);
     }
@@ -88,9 +98,9 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
 
-        move();
         Jump();
-
+        move();
+        _Grounded = false;
     }
 
     void SimulateJump(bool buttonDown, bool buttonUp)
@@ -105,7 +115,7 @@ public class PlayerMovement : MonoBehaviour
             jumpBufferCounter -= Time.deltaTime;
         }
 
-        if ((jumpBufferCounter > 0f) && IsGrounded())
+        if ((jumpBufferCounter > 0f) && _Grounded)
         {
             _jump = true;
         }
@@ -122,7 +132,7 @@ public class PlayerMovement : MonoBehaviour
         {
             jumpBufferCounter = 0f;
             // rb.linearVelocity += Vector3.up * jump;
-            rb.AddForce(Vector3.up * jump, ForceMode.Impulse);
+            rb.AddForce(contactNormal * jump, ForceMode.Impulse);
             _jump = false;
         }
         if (_jumpReleased)
@@ -184,9 +194,17 @@ public class PlayerMovement : MonoBehaviour
 
         bool result = Physics.SphereCast(ray, sphereCollider.bounds.extents.y, out rayHit, extraHeightTest);
 
-        if (result && _jump == false)
+        result = result && rayHit.normal.y >= minGroundDotProduct;
+
+        if (result)
         {
-            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+            contactNormal = rayHit.normal.normalized;
+            if (_jump == false)
+                rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        }
+        else
+        {
+            contactNormal = Vector3.up;
         }
 
         Debug.Log(rayHit.collider);
@@ -196,9 +214,25 @@ public class PlayerMovement : MonoBehaviour
     void move()
     {
         rb.AddForce(movement);
+        float decceleration;
 
+        Vector3 hVel = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+        if (hVel.magnitude > maxSpeed)
+        {
+            hVel = hVel.normalized * maxSpeed;
+            rb.linearVelocity = new Vector3(hVel.x, rb.linearVelocity.y, hVel.z);
+        }
+
+        if (_Grounded)
+        {
+            decceleration = groundDecceleration;
+        }
+        else
+        {
+            decceleration = airDecceleration;
+        }
         rb.linearVelocity = Vector3.MoveTowards(rb.linearVelocity, new Vector3(0f, rb.linearVelocity.y, 0f),
-            decceleration * Time.fixedDeltaTime);
+           decceleration * Time.fixedDeltaTime);
     }
 
     void DebugPause()
